e32abdd4f12c34ea7a57935f185accfb49b0b1fa
Bharat Shah <akashche72@gmail.com>
2024-03-08 17:35:35 +0400

    copied assets

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import('./bootstrap').then(
+  ({ mount }) => {
+    const localRoot = document.getElementById('auth');
+
+    mount({
+      mountPoint: localRoot!,
+      routingStrategy: 'browser',
+    });
+  }
+);
+
+export {};
