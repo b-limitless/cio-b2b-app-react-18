10624e69da645634d6893952b7311a969ef859c3
Bharat Shah <akashche72@gmail.com>
2024-03-14 17:05:43 +0400

    formatting code

--- auth/src/bootstrap.tsx
+++ auth/src/bootstrap.tsx
@@ -1,49 +1,36 @@
-import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
 import { createRouter } from "./routing/router-factory";
 import { RoutingStrategy } from "./routing/types";
-import "./index.css";
-import { Provider, useDispatch } from "react-redux";
 import { Store } from "./store";
-import { updateNavigation } from "./reducers/navigationSlice";
 
 
-const DispatchNavigation = ({data}:{data:any}) => {
-  const [auth, seAuth] = useState<boolean>();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    seAuth(true);
-  }, []);
-
-  return null;
-}
 const mount = ({
   mountPoint,
   initialPathname,
   routingStrategy,
-  setAuth, 
+  setAuth,
 }: {
   mountPoint: HTMLElement;
   initialPathname?: string;
   routingStrategy?: RoutingStrategy;
-  setAuth:Function;
-  navigateFromCell:Function;
+  setAuth: Function;
+  navigateFromCell: Function;
 }) => {
-  
-  const router = createRouter({ 
-            strategy: routingStrategy, 
-            initialPathname, 
-            setAuth
-          }
-         );
+
+  const router = createRouter({
+    strategy: routingStrategy,
+    initialPathname,
+    setAuth
+  }
+  );
   const root = createRoot(mountPoint);
 
 
   root.render(
     <Provider store={Store}>
-      {/* <DispatchNavigation data={useNavigationFromShell}/> */}
       <RouterProvider router={router} />
     </Provider>
   );
