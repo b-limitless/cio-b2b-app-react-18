f20b3e40b37e9805865748583c7b1af048f343c2
Bharat Shah <akashche72@gmail.com>
2024-04-23 19:45:08 +0400

    finishing the issues

--- auth/src/components/VerifyRegisteredAccount.tsx
+++ auth/src/components/VerifyRegisteredAccount.tsx
@@ -12,20 +12,7 @@ export default function VerifyRegisteredAccount({navigateFromCell}: IVerifyRegis
   return (
     <Template>
       <div className='right col'>
-        <div className='group-nav'>
-          <div className='row navigate'>
-            <span className='ico-back'>
-              <span className='icon'>
-                <BackLeftIcon />
-              </span>
-              <div className='back'>Back</div>
-            </span>
-            <span className='steps-info'>
-              <span className='step'>STEP 01/03</span>
-              <span className='info'>Personal Info.</span>
-            </span>
-          </div>
-        </div>
+        
         <VerifyFeature navigateFromCell={navigateFromCell}/>
       </div>
     </Template>
