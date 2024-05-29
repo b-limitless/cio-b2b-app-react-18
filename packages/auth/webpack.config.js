773e365198e634c13f7d2de524153bb9e810c44f
Bharat Shah <akashche72@gmail.com>
2024-05-29 17:28:24 +0400

    adding script to check if we are in production so building with domain name or use localhost as domain

--- packages/auth/webpack.config.js
+++ packages/auth/webpack.config.js
@@ -5,94 +5,100 @@ const path = require("path");
 const port = 8084;
 
 const deps = require("./package.json").dependencies;
-module.exports = {
-  output: {
-    publicPath: `http://localhost:${port}/`,
-  },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-
-  devServer: {
-    port: port,
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  return {
+    output: {
+      publicPath: isProduction
+        ? "http://www.ensemblecrafts.com/auth/latest"
+        : `http://localhost:${port}/`,
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+
+    devServer: {
+      port: port,
+      historyApiFallback: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
       },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(woff2?|jpe?g|png|gif|ico)$/,
-        oneOf: [
-          {
-            include: path.resolve(__dirname, "../node_modules/"),
-            use: "svg-inline-loader",
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
           },
-          {
-            exclude: path.resolve(__dirname, "../node_modules/"),
-            use: "url-loader",
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
           },
-        ],
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      }
+        },
+        {
+          test: /\.(woff2?|jpe?g|png|gif|ico)$/,
+          oneOf: [
+            {
+              include: path.resolve(__dirname, "../node_modules/"),
+              use: "svg-inline-loader",
+            },
+            {
+              exclude: path.resolve(__dirname, "../node_modules/"),
+              use: "url-loader",
+            },
+          ],
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ["@svgr/webpack"],
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "auth",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./AuthApp": "./src/bootstrap",
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+          "@reduxjs/toolkit": {
+            singleton: true,
+            requiredVersion: deps["@reduxjs/toolkit"],
+          },
+          "react-router-dom": {
+            singleton: true,
+            requiredVersion: deps["react-router-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "auth",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        './AuthApp': './src/bootstrap',
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-        "@reduxjs/toolkit": {
-          singleton: true,
-          requiredVersion: deps["@reduxjs/toolkit"],
-        }, 
-        "react-router-dom" : {
-          singleton: true,
-          requiredVersion: deps["react-router-dom"],
-        }
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  };
 };
