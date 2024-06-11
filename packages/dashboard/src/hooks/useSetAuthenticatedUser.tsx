f60a300d35397658ffcccb3536c96b94dc2e4626
Bharat Shah <akashche72@gmail.com>
2024-06-11 14:32:38 +0400

    redirecting user to the auth page in dahsboard if they are not authenticated

--- packages/dashboard/src/hooks/useSetAuthenticatedUser.tsx
+++ packages/dashboard/src/hooks/useSetAuthenticatedUser.tsx
@@ -1,9 +1,13 @@
+// If this is in production and in isolated service 
+// We are redirecting the user to the another domain such as auth.domain.com
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { APIS } from '../apis';
 import { authenticatedUser } from '../reducers/authSlice';
 import { RootState } from '../store';
 import { request } from '../utils/request';
+import { isDev } from '../env';
+import { redirectToSignin } from '../functions/reDirectTo';
 
 interface AuthenticatedUserInterface {
     setLoading: Function;
@@ -25,7 +29,15 @@ export default function useSetAuthenticatedUser({ setLoading, navigateFromCell }
                 
             } catch (err) {
                 console.error('Count not fetch current user', err);
-                navigateFromCell();
+                if(navigateFromCell) {
+                    navigateFromCell();
+                }
+
+                
+                if(!isDev()) {
+                    redirectToSignin();
+                }
+                
                 
             }
             setLoading(false);
